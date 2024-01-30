64cf8beecf57a43f3858cff6df45d06fc540cfa8
Shakir Ali <shakirali9749@gmail.com>
2024-01-30 20:02:48 +0500

    add change the solutions of two sum

--- twosum.py
+++ twosum.py
@@ -1,16 +1,11 @@
-def twosum(nums,target):
-	for i in range(len(nums)):
-		for j in range(i+1,len(nums)):
-			if target-nums[i] == nums[j]:
-				# 26-11 == 14 
-				# 26-11 == 15(15 == 15)
-				return[i,j]
-	return None
+def twosum(nums, target):
+    for i in range(len(nums)): #first loop for the first element and soon
+        for j in range(i + 1, len(nums)):
+            if (nums[i] != nums[j]) and nums[i] + nums[j] == target:
+                return [i, j]
+    return "Not found"
 
+nums = [22,21,34,4,5]
+target = 9
 
-test = [2,7,11,14,15]
-target = 26
-print(twosum(test,target))	
-
-# index starts from zero
-# length starts from one
+print(twosum(nums,target))
