44841e6c0489b9efbdce46f25415c9c5f0ada1d7
Shakir Ali <shakirali9749@gmail.com>
2024-09-27 12:29:29 +0500

    count lenth of last world from the given string

---
+++ remove_zeroz.py
@@ -0,0 +1,16 @@
+"""
+remove zeroz from end othe the list 1
+and after that add list 2 in list1
+"""
+
+
+def remove_trailing_zeroz(list1,list2):
+    while list1[-1] == 0:
+        list1.pop()
+    list1 = list1 + list2
+    list1.sort()
+    return list1
+
+num1 = [1,2,3,0,0,0,0] 
+num2 = [1,2,3,]
+print(remove_trailing_zeroz(num1,num2))
